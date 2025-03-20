95320ed1bfd4606bb5ff1904028a4deec0139b24
Mohamad Abed Al Houssein <125250165+Mohamad1234ALi@users.noreply.github.com>
2025-03-20 15:30:29 +0200

    Update app-vector.py

--- app-vector.py
+++ app-vector.py
@@ -46,9 +46,9 @@ label_encoders = {
 }
 
 
-# Load MinMaxScaler (these must match your stored embeddings)
-scaler = MinMaxScaler(feature_range=(0, 1))
-scaler.fit(np.array([[2000, 1000, 0, 50], [2025, 100000, 300000, 1000]]))  # Adjust based on your dataset
+# Initialize StandardScaler
+scaler_url = "https://car-recommendation-raed.s3.us-east-1.amazonaws.com/modelvectoring/scaler.pkl"
+scaler = load_scaler(scaler_url)
 
 # Function to convert user input into vector
 def preprocess_input(category, gearbox, fuel_type, first_reg, price, mileage, performance):
@@ -56,8 +56,9 @@ def preprocess_input(category, gearbox, fuel_type, first_reg, price, mileage, pe
     gearbox_encoded = label_encoders["Gearbox"].transform([gearbox])[0]
     fuel_type_encoded = label_encoders["FuelType"].transform([fuel_type])[0]
 
-    numerical_values = np.array([[first_reg, price, mileage, performance]])
-    numerical_scaled = scaler.transform(numerical_values)[0] 
+    # numerical_values = np.array([[first_reg, price, mileage, performance]])
+    # numerical_scaled = scaler.transform(numerical_values)[0] 
+    numerical_scaled = scaler.transform([[first_reg, price, mileage, performance]])[0]
 
     return np.concatenate(([category_encoded, gearbox_encoded, fuel_type_encoded], numerical_scaled))
 
